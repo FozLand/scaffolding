4db9bfd7f56c8cd5bd1ede257f2b1adcab442748
Foz <fozolo@gmail.com>
2016-11-21 16:29:36 -1000

    Rewrite, relicense.

---
+++ README.md
@@ -0,0 +1,47 @@
+Scaffolding
+===========
+
+The Scaffolding Minetest mod adds instantly removeable, climbable nodebox
+frames.  The intent is to make it easier to build large projects without fly
+privledges.  If the dye mod is available, colored scaffolding is also added.
+
+Crafting:
+
+|       |       |       |
+| ----- | ----- | ----- |
+| stick |       | stick |
+|       | stick |       |
+| stick |       | stick |
+
+License of source code
+-------------------------------
+
+Scaffolding
+Copyright (C) 2014 DanteLives
+Copyright (C) 2016 John Cole
+
+Scaffolding is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Scaffolding is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Scaffolding.  If not, see <http://www.gnu.org/licenses/>.
+
+License of texture
+------------------
+
+textures/greyscale_wood.png
+Copyright (C) 2016 John Cole
+
+Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+http://creativecommons.org/licenses/by-sa/3.0/
+
+Texture greyscale_wood.png is an adaptation of
+minetest_game/mods/default/textures/default_wood.png by BlockMen under CC BY-SA
+3.0.
--- init.lua
+++ init.lua
@@ -1,126 +1,104 @@
+--[[
+	Copyright (C) 2014 DanteLives
+	Copyright (C) 2016 John Cole
+	This file is part of Scaffolding.
+
+	Scaffolding is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Scaffolding is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Scaffolding.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+local def = {
+	description = 'Scaffolding',
+	tiles = {'default_wood.png'},
+	drawtype = 'nodebox',
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	node_box = {
+		type = 'fixed',
+		fixed = {
+			{ -1/2, -1/2, -1/2, -3/8, -3/8,  1/2 },
+			{ -1/2,  1/2, -1/2, -3/8,  3/8,  1/2 },
+			{  1/2,  1/2, -1/2,  3/8,  3/8,  1/2 },
+			{  1/2, -1/2, -1/2,  3/8, -3/8,  1/2 },
+			{ -1/2, -1/2, -1/2,  1/2, -3/8, -3/8 },
+			{ -1/2,  3/8, -1/2,  1/2,  1/2, -3/8 },
+			{ -1/2, -1/2, -1/2, -3/8,  1/2, -3/8 },
+			{  3/8, -1/2, -1/2,  1/2,  1/2, -3/8 },
+			{ -1/2, -1/2,  3/8,  1/2, -3/8,  1/2 },
+			{ -1/2,  3/8,  3/8,  1/2,  1/2,  1/2 },
+			{ -1/2, -1/2,  3/8, -3/8,  1/2,  1/2 },
+			{  3/8, -1/2,  3/8,  1/2,  1/2,  1/2 },
+		},
+	},
+	climbable = true,
+	walkable = false,
+	is_ground_content = true,
+	groups = {scaffolding = 1, dig_immediate = 3, flammable = 3},
+	sounds = default.node_sound_wood_defaults()
+}
+
+minetest.register_node('scaffolding:scaffolding', def)
 
 minetest.register_craft({
 	output = 'scaffolding:scaffolding',
 	recipe = {
-		{'default:stick', '', 'default:stick'},
-		{'', 'default:stick', ''},
-		{'default:stick', '', 'default:stick'},
+		{'default:stick', '',              'default:stick'},
+		{'',              'default:stick', ''             },
+		{'default:stick', '',              'default:stick'},
 	}
 })
 
-minetest.register_node("scaffolding:scaffolding", {
-	description = "scaffolding",
-	tiles = {"default_wood.png"},
-	drawtype = "nodebox",
-    	paramtype = "light",
-    	paramtype2 = "facedir",
-	node_box = {
-        	type = "fixed",
-        	fixed = {
-            	{ -0.5, -0.5, -0.5, -0.4,  -0.4, 0.5 },
-            	{  -0.5, 0.5, -0.5,  -0.4,  0.4, 0.5 },
-            	{  0.5,  0.5, -0.5,  0.4,  0.4,  0.5 },
-            	{  0.5, -0.5, -0.5,  0.4, -0.4,  0.5 },
-		{ -0.5, -0.5, -0.5,  0.5, -0.4, -0.4 },
-		{ -0.5,  0.4, -0.5,  0.5,  0.5, -0.4 },
-		{ -0.5, -0.5, -0.5, -0.4,  0.5, -0.4 },
-		{  0.4, -0.5, -0.5,  0.5,  0.5, -0.4 },
-		{ -0.5, -0.5,  0.4,  0.5, -0.4,  0.5 },
-		{ -0.5,  0.4,  0.4,  0.5,  0.5,  0.5 },
-		{ -0.5, -0.5,  0.4, -0.4,  0.5,  0.5 },
-		{  0.4, -0.5,  0.4,  0.5,  0.5,  0.5 },      	
-            },
-	},
-	climbable = true,
-	walkable = false,
-	is_ground_content = true,
-	groups = {scaffolding=1,dig_immediate=3,flammable=3},
-	sounds = default.node_sound_wood_defaults()
-})
-
-local scaffolding = {}
--- This uses a trick: you can first define the recipes using all of the base
--- colors, and then some recipes using more specific colors for a few non-base
--- colors available. When crafting, the last recipes will be checked first.
-scaffolding.dyes = {
-	{"white",      "White",      "basecolor_white"},	
-	{"red",        "Red",        "basecolor_red"},
-	{"yellow",     "Yellow",     "basecolor_yellow"},
-	{"blue",       "Blue",       "basecolor_blue"},
-	{"orange",     "Orange",     "excolor_orange"},
-	{"violet",     "Violet",     "excolor_violet"},	
-	{"grey",       "Grey",       "basecolor_grey"},
-	{"darkgrey",  "Dark grey",  "excolor_darkgrey"},
-	{"black",      "Black",      "basecolor_black"},
-	{"cyan",       "Cyan",       "basecolor_cyan"},
-	{"green",      "Green",   "basecolor_green"},
-	{"magenta",    "Magenta dye",   "basecolor_magenta"},
-}
-
-for _, row in ipairs(scaffolding.dyes) do
-	local name = row[1]
-	local desc = row[2]
-	local craft_color_group = row[3]
-	-- Node Definition
-	minetest.register_node("scaffolding:scaffolding_"..name, {
-		description = desc.." scaffolding",
-		tiles = {"coloredwood_wood_"..name..".png"},
-		drawtype = "nodebox",
-    	paramtype = "light",
-    	paramtype2 = "facedir",
-	node_box = {
-        	type = "fixed",
-        	fixed = {
-            	{ -0.5, -0.5, -0.5, -0.4,  -0.4, 0.5 },
-            	{  -0.5, 0.5, -0.5,  -0.4,  0.4, 0.5 },
-            	{  0.5,  0.5, -0.5,  0.4,  0.4,  0.5 },
-            	{  0.5, -0.5, -0.5,  0.4, -0.4,  0.5 },
-		{ -0.5, -0.5, -0.5,  0.5, -0.4, -0.4 },
-		{ -0.5,  0.4, -0.5,  0.5,  0.5, -0.4 },
-		{ -0.5, -0.5, -0.5, -0.4,  0.5, -0.4 },
-		{  0.4, -0.5, -0.5,  0.5,  0.5, -0.4 },
-		{ -0.5, -0.5,  0.4,  0.5, -0.4,  0.5 },
-		{ -0.5,  0.4,  0.4,  0.5,  0.5,  0.5 },
-		{ -0.5, -0.5,  0.4, -0.4,  0.5,  0.5 },
-		{  0.4, -0.5,  0.4,  0.5,  0.5,  0.5 },      	
-            },
-	},
-	climbable = true,
-	walkable = false,
-	is_ground_content = true,
-	groups = {scaffolding=1,dig_immediate=3,flammable=3},
-	sounds = default.node_sound_wood_defaults()
-	})
-	if craft_color_group then
-		minetest.register_craft({
-			type = "shapeless",
-			output = 'scaffolding:scaffolding_'..name,
-			recipe = {'group:'..craft_color_group, 'group:scaffolding'},
-		
-		})
-	
-	end
-	minetest.register_craft({
-	output = 'scaffolding:scaffolding_'..name,
-	recipe = {
-		{'coloredwood:stick_'..name, '', 'coloredwood:stick_'..name},
-		{'', 'coloredwood:stick_'..name, ''},
-		{'coloredwood:stick_'..name, '', 'coloredwood:stick_'..name},
-		}
-
-	})
-
-
-
-end
-
-
-
-
-
 minetest.register_craft({
-	type = "fuel",
-	recipe = "group:scaffolding",
+	type = 'fuel',
+	recipe = 'group:scaffolding',
 	burntime = 15,
 })
 
+if dye then
+	local dyes = {
+		{"white",      "White",      'unicolor_white',       '#FFFFFF:191'},
+		{"grey",       "Grey",       'unicolor_grey',        '#686868:223'},
+		{"dark_grey",  "Dark grey",  'unicolor_darkgrey',    '#404040:239'},
+		{"black",      "Black",      'unicolor_black',       '#161616:239'},
+		{"violet",     "Violet",     'unicolor_violet',      '#5100A2:239'},
+		{"blue",       "Blue",       'unicolor_blue',        '#2626FF:215'},
+		{"cyan",       "Cyan",       'unicolor_cyan',        '#26FFFF:191'},
+		{"dark_green", "Dark green", 'unicolor_dark_green',  '#085101:239'},
+		{"green",      "Green",      'unicolor_green',       '#00A200:215'},
+		{"yellow",     "Yellow",     'unicolor_yellow',      '#E4E400:215'},
+		{"brown",      "Brown",      'unicolor_dark_orange', '#512900:239'},
+		{"orange",     "Orange",     'unicolor_orange',      '#A25100:239'},
+		{"red",        "Red",        'unicolor_red',         '#A20000:223'},
+		{"magenta",    "Magenta",    'unicolor_red_violet',  '#A200A2:223'},
+		{"pink",       "Pink",       'unicolor_light_red',   '#FF8080:239'},
+	}
+
+	for _, row in ipairs(dyes) do
+		local name  = row[1]
+		local desc  = row[2]
+		local craft_color_group = row[3]
+		local color = row[4]
+
+		local def = table.copy(def)
+		def.description = desc..' Scaffolding'
+		def.tiles = {'scaffolding_greyscale_wood.png^[colorize:'..color}
+
+		minetest.register_node('scaffolding:scaffolding_'..name, def)
+
+		minetest.register_craft({
+			type = 'shapeless',
+			output = 'scaffolding:scaffolding_'..name,
+			recipe = {'group:'..craft_color_group, 'group:scaffolding'},
+		})
+	end
+end
